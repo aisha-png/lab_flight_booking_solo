7ace61f5e7ab085961ce1add4f7181b28a16a70a
Aisha Mohamed <aisha_mo@hotmail.com>
2022-05-08 21:15:30 +0100

    initialised addANewFlight

--- src/main/java/CLI.java
+++ src/main/java/CLI.java
@@ -34,20 +34,38 @@ public class CLI {
                     "What would you like to do" + System.lineSeparator() +
                             "Please select from the following:" + System.lineSeparator() +
                             "--------------------------------------------------" + System.lineSeparator() +
-                            "  0   Exit" + System.lineSeparator() +
                             "  1   Book a flight" + System.lineSeparator() +
                             "  2   Display all available flights" + System.lineSeparator() +
                             "  3   Cancel a flight" + System.lineSeparator() +
                             "  4   Add a new passenger onto a flight" + System.lineSeparator() +
-                            "  5   Remove a passenger from a flight";
+                            "  5   Remove a passenger from a flight"+ System.lineSeparator() +
+                            "  6   Exit" + System.lineSeparator();
             System.out.println(instructions);
             int userInput = scanner.nextInt();
 
-            String response = "You selected " + userInput;
+            String response = "You selected " + userInput + " - Book a flight";
             System.out.println(response);
 
-
+            switch(userInput){
+                case 1:
+                    System.out.println("\nAction: Book a flight");
+                    addANewFlight();
+                    break;
+                case 6:
+                    System.out.println("\nThank you for ");
+            }
         }
+    }
+
+    public void addANewFlight(){
+        System.out.println("Please enter the destination you wish to get to: ");
+        scanner.nextLine();
+        String input= scanner.nextLine();
+        String answer = "You selected " + input.toUpperCase();
+        System.out.println(answer);
+
+
+
     }
 
 
