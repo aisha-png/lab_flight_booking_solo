77728273d5ac7aba8d86a71fc6a900edef4b2fec
Aisha Mohamed <aisha_mo@hotmail.com>
2022-05-08 21:52:50 +0100

    last case exit command works

--- src/main/java/CLI.java
+++ src/main/java/CLI.java
@@ -4,25 +4,25 @@ import java.util.Scanner;
 
 public class CLI {
     private Scanner scanner;
-    private List<FlightList> allAvailableFlights;
+    private List<FlightList> flights;
     private List<PassengerList> allPassengers;
     private boolean EXIT;
 
     public CLI(){
         this.scanner = new Scanner(System.in);
-        this.allAvailableFlights = new ArrayList<>();
+        this.flights = new ArrayList<>();
         this.allPassengers = new ArrayList<>();
     }
 
 //    Getting all passengers and flights
 
-    public List<FlightList> getAllAvailableFlights(FlightList flights){
-        return allAvailableFlights;
-    }
-
-    public List<PassengerList> getAllPassengers(){
-        return allPassengers;
-    }
+//    public List<FlightList> getAllAvailableFlights(){
+//        return allAvailableFlights;
+//    }
+//
+//    public List<PassengerList> getAllPassengers(){
+//        return allPassengers;
+//    }
 
 //    CLI
 
@@ -39,31 +39,39 @@ public class CLI {
                             "  3   Cancel a flight" + System.lineSeparator() +
                             "  4   Add a new passenger onto a flight" + System.lineSeparator() +
                             "  5   Remove a passenger from a flight"+ System.lineSeparator() +
-                            "  6   Exit" + System.lineSeparator();
+                            "  6   Exit" + System.lineSeparator() +
+                            "--------------------------------------------------" + System.lineSeparator() +
+                            "  Please select a number between 1 and 6" + System.lineSeparator() +
+                            "--------------------------------------------------" + System.lineSeparator();
             System.out.println(instructions);
             int userInput = scanner.nextInt();
 
-            String response = "You selected " + userInput + " - Book a flight";
-            System.out.println(response);
 
             switch(userInput){
                 case 1:
+                    String response = "You selected " + userInput + " - Book a flight";
+                    System.out.println(response);
                     System.out.println("\nAction: Book a flight");
                     addANewFlight();
                     break;
                 case 6:
-                    System.out.println("\nThank you for ");
+                    String response2 = "You selected " + userInput + " - Exit - ";
+                    System.out.println(response2);
+                    System.out.println("\nThank you for using our service");
+                    EXIT = true;
+                    break;
             }
         }
     }
 
     public void addANewFlight(){
-        System.out.println("Please enter the destination you wish to get to: ");
+        System.out.println("Please enter the destination you wish to travel to: ");
         scanner.nextLine();
         String input= scanner.nextLine();
         String answer = "You selected " + input.toUpperCase();
         System.out.println(answer);
-
+        //need to initialise a method that adds a flight onto
+        String destination = scanner.nextLine();
 
 
     }
